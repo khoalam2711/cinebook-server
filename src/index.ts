29657b876eaa4246793955031a8bb178ff0f6f8d
KoahF <khoakg2013@gmail.com>
2023-06-08 14:18:16 +0700

    Add helmet, google oauth client config

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
 import https from 'https';
 import fs from 'fs';
 import express from 'express';
+import helmet from 'helmet';
+import 'dotenv/config'
+
 import AuthRouter from './routes/auth/auth.router';
 
-const PORT = 3000;
+const PORT = 8000;
+const config = {
+	CLIENT_ID: process.env.CLIENT_ID,
+	CLIENT_SECRET: process.env.CLIENT_SECRET,
+};
+
 const app = express();
 
+app.use(helmet());
 app.use(express.json());
 
 app.use('/auth', AuthRouter);
