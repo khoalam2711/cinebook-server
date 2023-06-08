d8b946c01dddf7d77ee29e4bdfd873029790e49a
KoahF <khoakg2013@gmail.com>
2023-06-08 16:52:55 +0700

    Implement base oauth google authenticate flow

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,9 @@ import https from 'https';
 import fs from 'fs';
 import express from 'express';
 import helmet from 'helmet';
-import 'dotenv/config'
+import passport, { Profile } from 'passport';
+import { Strategy, VerifyCallback } from 'passport-google-oauth20';
+import 'dotenv/config';
 
 import AuthRouter from './routes/auth/auth.router';
 
@@ -11,10 +13,26 @@ const config = {
 	CLIENT_ID: process.env.CLIENT_ID,
 	CLIENT_SECRET: process.env.CLIENT_SECRET,
 };
+function verifyCallback(accessToken: string, refreshToken: string, profile: Profile, done: VerifyCallback) {
+	console.log('Google profile', profile);
+	done(null, profile);
+}
+
+passport.use(
+	new Strategy(
+		{
+			clientID: config.CLIENT_ID as string,
+			clientSecret: config.CLIENT_SECRET as string,
+			callbackURL: '/auth/google/callback',
+		},
+		verifyCallback
+	)
+);
 
 const app = express();
 
 app.use(helmet());
+app.use(passport.initialize());
 app.use(express.json());
 
 app.use('/auth', AuthRouter);
--- src/routes/auth/auth.router.ts
+++ src/routes/auth/auth.router.ts
@@ -1,8 +1,24 @@
 import { Router } from 'express';
 import { httpHandleLogin } from './auth.controller';
+import passport from 'passport';
 
 const router = Router();
 
 router.get('/login', httpHandleLogin);
+router.get(
+	'/google',
+	passport.authenticate('google', {
+		scope: ['email', 'profile'],
+	})
+);
+
+router.get(
+	'/google/callback',
+	passport.authenticate('google', {
+		failureRedirect: '/failure',
+		successRedirect: '/',
+		session: false,
+	})
+);
 
 export default router;
